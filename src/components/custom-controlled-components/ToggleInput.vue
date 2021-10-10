9ec5151d79d4e598fdb2956f937513573f787765
Airat <parkourtes@gmail.com>
2021-10-10 20:32:46 +0300

    lesson 2

---
+++ src/components/custom-controlled-components/ToggleInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <span class="toggle" role="checkbox" tabindex="0"
+    @click="toggle"
+    @keydown.space.prevent="toggle"
+    :aria-checked="toggled.toString()"
+  ></span>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Model } from "vue-property-decorator"
+
+@Component({})
+export default class extends Vue {
+	@Model("toggle") readonly toggled: boolean
+  
+	toggle() {
+		this.$emit("toggle", !this.toggled)
+	}
+}
+</script>

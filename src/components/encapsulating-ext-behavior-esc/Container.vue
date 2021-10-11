75222264dd6876c3c5c4cd98e631a7a26f278487
Airat <parkourtes@gmail.com>
2021-10-11 18:58:29 +0300

    lesson 5

---
+++ src/components/encapsulating-ext-behavior-esc/Container.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="min-h-screen bg-grey-darker p-8">
+      <div class="max-w-sm mx-auto">
+        <user-settings-form :account-id="accountId"></user-settings-form>
+      </div>
+    </div>
+
+    <portal-target name="modals"></portal-target>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator"
+import UserSettingsForm from './UserSettingsForm.vue'
+
+@Component({
+  components: {
+    UserSettingsForm,
+  }
+})
+export default class extends Vue {
+  accountId = 7
+}
+</script>
---
+++ src/components/encapsulating-ext-behavior-esc/DeleteAccountButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button type="button" class="text-red-dark hover:underline"
+    @click="confirmDeleteModalOpen = true"
+  >
+    Delete Account
+    <portal to="modals">
+      <confirm-delete-modal
+        :show="confirmDeleteModalOpen"
+        @close="confirmDeleteModalOpen = false"
+        :account-id="accountId"
+      />
+    </portal>
+  </button>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator"
+import ConfirmDeleteModal from "./ConfirmDeleteModal.vue"
+
+@Component({
+  components: {
+    ConfirmDeleteModal
+  }
+})
+export default class extends Vue {
+	@Prop() readonly accountId: number
+	confirmDeleteModalOpen = false
+}
+</script>
